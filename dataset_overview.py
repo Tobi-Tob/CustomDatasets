dd2a12eef48479db4fb732a1d7a5e2b56e2145a3
KingEsel1 <reutermarkus1@gmail.com>
2024-01-12 15:13:33 +0100

    Completed Script for info of datasets

--- dataset_overview.py
+++ dataset_overview.py
@@ -1,12 +1,51 @@
-import os
+import os, csv
 
-# Set your dataset root directory and output filename here
+# Settings
 root_dir = "./"
-output_filename = "overview.txt"
+datasets = ["Common_Objects", "Road_Signs"]
+sub_dirs = ["test", "train-val"]
 
 
-def collect_datasets(root):
-    return []
+def get_info(dataset_path, dataset_name):
+    classes = {f'{dataset_name}': [0, 0]}
+
+    def count_jpg(split_name):
+        if split_name == "train-val":
+            split_dir = os.path.join(dataset_path, split_name)
+            split = 0
+        elif split_name == "test":
+            split_dir = os.path.join(dataset_path, split_name)
+            split = 1
+        else:
+            raise AttributeError(f"split is not train-val or test, but: {split_name}")
+
+        for class_name in os.listdir(split_dir):
+            path = os.path.join(split_dir, class_name)
+            if os.path.isdir(path):
+                if class_name not in classes.keys():
+                    classes[class_name] = [0, 0]
+                jpg_count = sum(1 for file in os.listdir(path) if file.lower().endswith('.jpg'))
+                classes[class_name][split] = jpg_count
+                classes[f'{dataset_name}'][split] += jpg_count  # add count of class to total of dataset
+
+    count_jpg("train-val")
+    count_jpg("test")
+    return classes
+
+
+def write_info(filename, d):
+    # Open the file in write mode
+    with open(filename, 'w', newline='') as file:
+        # Create a writer object specifying the fieldnames
+        fieldnames = ['dataset/class', 'train-val', 'test']
+        writer = csv.writer(file)
+        writer.writerow(fieldnames)
+
+        for key, values in data.items():
+            row = [key, str(values[0]), str(values[1])]
+            writer.writerow(row)
+
+    print(f"Data written to {filename}")
 
 
 def count_jpg_images(dataset_dir):
@@ -38,14 +77,8 @@ def save_counts_to_file(counts, output_file):
             f.write("\n")
 
 
-# Collect datasets
-datasets = collect_datasets(root_dir)
-
-# Count images within each dataset
-dataset_dirs = [os.path.join(root_dir, dataset) for dataset in datasets]
-image_counts = [count_jpg_images(dataset_dir) for dataset_dir in dataset_dirs]
-
-# Save results to a text file
-[save_counts_to_file(image_count, output_filename) for image_count in image_counts]
-
-print(f"Image counts saved to {output_filename}")
+if __name__ == "__main__":
+    for dataset in datasets:
+        data = get_info(os.path.join(root_dir, dataset), dataset)
+        write_info(f"{dataset}_info.csv", data)
+        print(data)
